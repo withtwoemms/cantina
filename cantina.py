baae6092939caed9e437a2a4568657bcb940a874
Emmanuel I. Obi <withtwoemms@gmail.com>
2016-08-09 18:05:12 -0500

    change app name to cantina

--- cantina.py
+++ cantina.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 def index():
     return "Hello, World"
 
-app.run(debug=True, host="0.0.0.0", port=8765)
+#app.run(debug=True, host="0.0.0.0", port=8765)
--- test_app.py
+++ test_app.py
@@ -1,10 +1,16 @@
-import app
+import cantina
 import unittest
 
 
 class FlaskAppTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = cantina.app.test_client()
+        cantina.app.config['TESTING'] = True
+
     def test_index_route(self):
-        self.assertEqual(0,1)
+        assert self.app.get('/')._status_code == 200
+        assert self.app.get('/').data == "Hello, World"
 
 if __name__ == "__main__":
     unittest.main()
